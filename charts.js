18f214575f914c3e2c186428d72f598b1613140b
Chris Olwell <chrisolwell@Chriss-MacBook-Pro.local>
2022-03-22 20:49:26 -0500

    pushing.

--- charts.js
+++ charts.js
@@ -59,7 +59,7 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
   var samples = data.samples;
-  
+  console.log(samples);
     // 4. Create a variable that filters the samples for the object with the desired sample number.
   var filteredSamples = metadata.filter(allSamples) => 
     //  5. Create a variable that holds the first sample in the array.
